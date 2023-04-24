53cef803a5291518593fb9c61b2a55abbd33c993
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-24 22:12:43 +0200

    Create App and update Display_items classes

---
+++ classes/app.rb
@@ -0,0 +1,9 @@
+class App
+  attr_reader :books, :music_albums, :games
+
+  def initialize
+    @books = []
+    music_albums = []
+    games = []
+  end
+end
--- classes/display_items.rb
+++ classes/display_items.rb
@@ -1,37 +1,34 @@
+require_relative 'app'
 
 class Display_items
-  puts "Welcome to our catalog app:" 
-  puts "1 Option 1" 
-  puts "2 Option 2" 
-  puts "3 Option 3" 
-  puts "4 Quit" 
-  
-  print "Enter your choice:"
-  choice = gets.chomp.to_i
-  
-  case choice
-  
-  when 1
-    option_1
-  when 2
-    option_2
-  when 3
-    option_3
-  when 4
-   exit
-  else
-    puts "Invalid choice"
+  def initialize
+    @app = App.new
   end
 
-  def option_1
-    #option 1
-    puts "Hello"
-
+  def choose_option
+    puts '1. List all books'
+    puts '2. List all music albums'
+    puts '3. List all games'
+    puts '4. List all genres'
+    puts '5. List all labels'
+    puts '6. List all authors'
+    puts '7. Add a book'
+    puts '8. Add a music album'
+    puts '9. Add a game'
+    puts '10. Exit'
   end
-  def option_2
-    #option 2
-  end
-  def option_3
-    #option 3
+  
+  def do_action(input)
+    case input
+    when 1 then @app.display_books
+    when 2 then @app.display_music_albums
+    when 3 then @app.display_games
+    when 4 then @app.display_genres
+    when 5 then @app.display_labels
+    when 6 then @app.display_authors
+    when 7 then @app.add_book
+    when 8 then @app.add_music_album
+    when 9 then @app.add_game
+    end
   end
 end
