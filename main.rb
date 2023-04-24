6934844446eea77728d2aa7d567ce265c55a623f
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-24 22:17:28 +0200

    Create entry-point

---
+++ main.rb
@@ -0,0 +1,19 @@
+require './classes/display_items'
+
+def main
+  choice = Display_items.new
+
+  loop do
+    choices.choose_option
+    input = gets.chomp.to_i
+
+    if input == 10
+      puts "Thanks for using the app, bye!"
+      break
+    end
+
+    choice.do_action(input)
+  end
+end
+
+main
