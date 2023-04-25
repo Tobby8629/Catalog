bd3bff500717233a36e14baf6179e56884067964
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-25 17:16:00 +0200

    Update label.rb

--- classes/label.rb
+++ classes/label.rb
@@ -1,2 +1,13 @@
-class Label < Item
+class Label
+  def initialize(id, name, color)
+    @id = id || Random.rand(1000)
+    @name = name
+    @color = color
+    @items = []
+  end
+
+  def add_label(item)
+    @items << item
+    item.add_label(self)
+  end
 end
