fdde5045f02f2a2ba9f30340f31c450e870e0821
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-25 19:38:45 +0200

    Add add_book  method in App class

--- classes/app.rb
+++ classes/app.rb
@@ -1,3 +1,6 @@
+require_relative 'book'
+require_relative 'label'
+
 class App
   attr_reader :books, :music_albums, :games
 
@@ -5,5 +8,25 @@ class App
     @books = []
     @music_albums = []
     @games = []
+    @labels = []
+  end
+
+  def add_book
+    puts ''
+    puts 'Enter the following book details'
+    puts '--------------------------------'
+    print 'Publisher: '
+    publisher = gets.chomp
+    print 'Publish date: '
+    publish_date = gets.chomp
+    print 'Cover state: '
+    cover_state = gets.chomp
+
+    new_book = Book.new(nil, publish_date, publisher, cover_state)
+    new_label = add_label
+    new_book.add_label(new_label)
+    @books << new_book
+    puts ''
+    puts 'Book added successfully!'
   end
 end
