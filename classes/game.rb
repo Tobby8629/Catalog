cac9479916ec5cc97c697e5a225a97a36f2081e0
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-26 16:13:59 +0200

    Add test for Author methods

--- classes/game.rb
+++ classes/game.rb
@@ -1,14 +1,15 @@
 require_relative './item'
+require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at)
+  def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    super && (DateTime.now - @last_played_at).to_i / 365 > 2
+    super && (Date.today - Date.parse(@last_played_at)).to_i / 365 > 2
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,27 +1,26 @@
 require_relative '../classes/game'
 
 describe Game do
-  let(:game) { Game.new(true, DateTime.now) }
+  let(:game1) { Game.new(1, '03-05-2005', true, '02-04-2020') }
+  let(:game2) { Game.new(2, '15-12-2021', false, '02-04-2022') }
 
   context 'attributes' do
     it 'has a multiplayer attribute' do
-      expect(game.multiplayer).to be true
+      expect(game1.multiplayer).to be true
     end
 
     it 'has a last played at attribute' do
-      expect(game.last_played_at).to be_a DateTime
+      expect(game1.last_played_at).to eq('02-04-2020')
     end
   end
 
   context '#can_be_archived?' do
     it 'returns true if 2 years have passed since last played' do
-      game.last_played_at = DateTime.now - (2 * 365)
-      expect(game.can_be_archived?).to be true
+      expect(game1.can_be_archived?).to be true
     end
 
     it 'returns false if 2 years have not passed since last played' do
-      game.last_played_at = DateTime.now - (1 * 365)
-      expect(game.can_be_archived?).to be false
+      expect(game2.can_be_archived?).to be false
     end
   end
 end
