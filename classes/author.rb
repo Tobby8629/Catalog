9714bef99d2312e3fc97792161ce32e4f65c721e
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-26 08:10:05 +0200

    Update author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,9 +1,8 @@
 require_relative './item'
-class Author < Item
+class Author
   attr_accessor :id, :first_name, :last_name, :items
 
   def initialize(first_name, last_name, id = Random.rand(1..1000))
-    super(id, publish_date)
     @id = id
     @first_name = first_name
     @last_name = last_name
