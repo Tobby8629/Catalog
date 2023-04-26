f7748d35eeec7b99a034fea46d736009bdbd4e44
tobby8629 <sarhmullah15@gmail.com>
2023-04-26 00:46:37 -0700

    changes on catalog

--- classes/app.rb
+++ classes/app.rb
@@ -86,6 +86,9 @@ class App
       puts
       nil
     else
+      puts ''
+    puts "Book list(#{@music_albums.length}):"
+    puts '--------------'
       @music_albums.each_with_index do |each, index|
         puts "[#{index}] #{each.name}, #{each.genre}, #{each.artist} published on #{each.publish_date}"
         puts ''
